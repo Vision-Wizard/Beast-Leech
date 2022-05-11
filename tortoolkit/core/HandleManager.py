6ac5fe21b4cdf167f00f66b9387a0423b203f941
Vision-Wizard <105115761+Vision-Wizard@users.noreply.github.com>
2022-05-11 10:31:44 +0530

    Update HandleManager.py

--- tortoolkit/core/HandleManager.py
+++ tortoolkit/core/HandleManager.py
@@ -644,7 +644,7 @@ async def set_password_zip(message):
 
 
 async def start_handler(event):
-    msg = "Hello This is TorToolkitX running on heroku an instance of <a href='https://github.com/KangersHub/TorToolkitX'>This Repo</a>. Try the repo for yourself and dont forget to put a STAR and fork."
+    msg = "🌞 Its a Leech Bot. Working for only <a href='https://t.me/RangoZex'>RANGO</a>"
     await event.reply(msg, parse_mode="html")
 
 
